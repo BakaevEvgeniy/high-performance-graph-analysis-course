f8904c3ebe28552c9790a0df55a2ce2eedac8548
Evgeniy Bakaev <evgeniy.bakaev@compvisionsys.com>
2023-03-22 19:45:11 +0300

    multi source bfs

--- project/bfs.py
+++ project/bfs.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from pygraphblas import Matrix, Vector
 from pygraphblas.types import BOOL, INT64
 from pygraphblas.descriptor import R, RC
@@ -23,3 +23,29 @@ def bfs(graph: Matrix, start_vertex: int) -> List[int]:
         step += 1
 
     return list(ans.vals)
+
+
+def multi_source_bfs(
+    graph: Matrix, start_vertices: List[int]
+) -> List[Tuple[int, List[int]]]:
+    n = graph.ncols
+    m = len(start_vertices)
+    q = Matrix.sparse(BOOL, nrows=m, ncols=n)
+    used = Matrix.sparse(BOOL, nrows=m, ncols=n)
+    ans = Matrix.dense(INT64, nrows=m, ncols=n, fill=-1)
+
+    for i, j in enumerate(start_vertices):
+        q.assign_scalar(True, i, j)
+        used.assign_scalar(True, i, j)
+        ans.assign_scalar(0, i, j)
+
+    step = 1
+    prev_nnz = -1
+    while used.nvals != prev_nnz:
+        prev_nnz = used.nvals
+        q.mxm(graph, mask=used, out=q, desc=RC)
+        used.eadd(q, BOOL.lor_land, out=used, desc=R)
+        ans.assign_scalar(step, mask=q)
+        step += 1
+
+    return [(vertex, list(ans[i].vals)) for i, vertex in enumerate(start_vertices)]
---
+++ tests/test_multi_source_bfs.py
@@ -0,0 +1,66 @@
+import pytest
+from pygraphblas import Matrix
+from pygraphblas.types import BOOL, INT64
+from project.bfs import multi_source_bfs
+
+
+@pytest.mark.parametrize(
+    "I, J, V, size, start_vertices, expected_ans",
+    [
+        (
+            [
+                0,
+                0,
+                1,
+                1,
+                3,
+                5,
+                5,
+            ],
+            [
+                1,
+                5,
+                2,
+                3,
+                5,
+                6,
+                4,
+            ],
+            [True] * 7,
+            7,
+            [0],
+            [(0 ,[0, 1, 2, 2, 2, 1, 2])],
+        ),
+        ([0], [0], [False], 1, [0], [(0, [0])]),
+        ([0, 1, 1], [1, 1, 2], [True, True, False], 3, [0], [(0, [0, 1, -1])]),
+        (
+            [
+                0,
+                0,
+                1,
+                1,
+                3,
+                3,
+                5,
+                5,
+            ],
+            [
+                1,
+                5,
+                2,
+                3,
+                2,
+                5,
+                6,
+                4,
+            ],
+            [True] * 8,
+            7,
+            [1, 3],
+            [(1, [-1, 0, 1, 1, 3, 2, 3]), (3, [-1, -1, 1, 0, 2, 1, 2])],
+        ),
+    ],
+)
+def test_multi_source_bfs(I, J, V, size, start_vertices, expected_ans):
+    graph = Matrix.from_lists(I, J, V, nrows=size, ncols=size)
+    assert multi_source_bfs(graph, start_vertices) == expected_ans
