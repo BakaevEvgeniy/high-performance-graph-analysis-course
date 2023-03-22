b9d64c48bfae8563af920a5807ca9fc3f1f97745
Evgeniy Bakaev <evgeniy.bakaev@compvisionsys.com>
2023-03-22 19:45:47 +0300

    codestyle fixes

--- project/utils.py
+++ project/utils.py
@@ -2,14 +2,13 @@ from networkx import MultiDiGraph, nx_agraph
 from pygraphblas import Matrix
 from pygraphblas.types import BOOL
 
+
 def read_graph(path: str) -> MultiDiGraph:
     return nx_agraph.read_dot(path)
 
 
 def graph_to_ajd_matrix(graph: MultiDiGraph) -> Matrix:
-    adj_matrix = Matrix.sparse(
-        BOOL, graph.number_of_nodes(), graph.number_of_nodes()
-    )
-    for (source, target) in graph.edges():
+    adj_matrix = Matrix.sparse(BOOL, graph.number_of_nodes(), graph.number_of_nodes())
+    for source, target in graph.edges():
         adj_matrix[int(source), int(target)] = True
     return adj_matrix
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,14 @@
 from project.utils import read_graph, graph_to_ajd_matrix
 
+
 def test_read_graph():
     graph = read_graph("./tests/test_graph.dot")
-    
+
     adj = graph_to_ajd_matrix(graph)
-    
+
     sources = [0, 0, 1, 2, 3, 4, 5]
     targets = [1, 3, 2, 0, 4, 5, 0]
-    
+
     assert adj.to_lists()[0] == sources
     assert adj.to_lists()[1] == targets
     assert adj.to_lists()[2] == [True] * 7
